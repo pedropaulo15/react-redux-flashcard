b7f9d8e200219ec9a92dccd20181d5fca377f637
Pedro <pedropaulo15@gmail.com>
2018-12-21 16:37:35 +0000

    Set the stack title and content dynamically with redux.

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; // Connects components to the store
 import { Link } from 'react-router-dom';
 
 class Stack extends Component {
   render() {
+    // console.log('stack props', this.props);
+    const { title, cards } = this.props.stack;
+
     return (
       <div>
         <Link to='/'>Home</Link>
-        <h3>example title</h3>
+        <h3>{title}</h3>
+        <br />
+        {
+          cards.map(card => {
+            return(
+              <div key={card.id}>
+                {card.prompt}
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
 }
 
-export default Stack;
+/** This function maps the state of the redux store, on the
+ * props of the this Stack Component.
+ * So the stack objects from the store can be accessible
+ * within this components throught this.props.
+ * 
+ * @example
+ * <h3>{this.props.stack.title}</h3>
+ * 
+ * @param state represents the redux store
+ */
+function mapStateToProps(state) {
+  return { stack: state } // this state is coming from the reducer
+}
+
+export default connect(mapStateToProps, null)(Stack);
--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; // Connects components to the store
 import stacks from '../data/stacks.json';
 import { Link } from 'react-router-dom';
+import { setStack } from '../actions';
 
 class StackList extends Component {
   render() {
+    // console.log('props', this.props);
     return (
       <div>
         {
           stacks.map(stack => {
             return(
-              <Link key={stack.id} to='/stack'>
+              <Link 
+                key={stack.id} 
+                to='/stack'
+                onClick={() => this.props.setStack(stack)}
+              >
                 <h4 key={stack.id}>{stack.title}</h4>
               </Link>
             )
@@ -20,4 +27,14 @@ class StackList extends Component {
   }
 }
 
-export default StackList;
+/** 
+ * Connects the setStack action with the props of the StackList
+ * component.
+ * 
+ * So by clicking on the link it will dispatch the setStack
+ * action.
+ * 
+ * @example onClick={() => this.props.setStack(stack)} See the Link
+ * above
+ */
+export default connect(null, { setStack })(StackList);
