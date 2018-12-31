5f47d0319eb9da88160a11bd9982e00770fe419a
Pedro <pedropaulo15@gmail.com>
2018-12-31 01:54:02 +0000

    Testing the Redux Reducers in FlashcardPro.

---
+++ src/reducers/index.test.js
@@ -0,0 +1,21 @@
+import rootReducer from './index';
+import * as actions from '../actions';
+import { stack, stacks } from '../data/fixtures';
+
+describe('root reducer', () => {
+  it('returns the initial state', () => {
+    expect(rootReducer({}, {})).toEqual({stack: {}, stacks: []});
+  });
+
+  it('sets the main stack', () => {
+    expect(rootReducer({}, { type: actions.SET_STACK, stack })).toEqual({ stack, stacks: [] });
+  });
+
+  it('loads stacks', () => {
+    expect(rootReducer({}, { type: actions.LOAD_STACKS, stacks})).toEqual({ stack: {}, stacks });
+  });
+
+  it('adds a new stack', () => {
+    expect(rootReducer({}, { type: actions.ADD_STACK, stack})).toEqual({stack: {}, stacks: [stack] });
+  });
+});
